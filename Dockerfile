379e7584de10422587c2b1d257b04a6c582c51d8
Leslie Yu <leslie_yu@asus.com>
2022-11-28 20:18:31 +0800

    Sync with android12-rockchip
    Change-Id: Idd179c5b22290c17ce85062adf28bbe69541d36f

--- Dockerfile
+++ Dockerfile
@@ -1,28 +1,11 @@
 FROM ubuntu:20.04
 
 ARG DEBIAN_FRONTEND=noninteractive
-ARG userid
-ARG groupid
-ARG username
 
-# Install required packages for building Tinker Board (S) Android N
 # kmod: depmod is required by "make modules_install"
 RUN apt-get update && \
     apt-get install -y make gcc python gcc-multilib bc lzop sudo m4 zip git \
     curl openjdk-8-jdk libxml2-utils kmod g++-multilib python3 libncurses5 \
     rsync
 
-RUN groupadd -g $groupid $username && \
-    useradd -m -u $userid -g $groupid $username && \
-    echo "$username ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-
-ENV HOME=/home/$username
-ENV USER=$username
-WORKDIR /source
-
-COPY entrypoint.sh /entrypoint.sh
-RUN chmod a+x /entrypoint.sh
-
 COPY java.security /etc/java-8-openjdk/security/java.security
-
-ENTRYPOINT ["/entrypoint.sh"]
--- docker-builder-run.sh
+++ docker-builder-run.sh
@@ -1,70 +1,14 @@
 #!/bin/bash
+#
+# Provide the Docker environment for ASUS IoT.
 
-set -xe
+export ASUS_DOCKER_ENV_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-start_time=$(date +"%s")
+source ${ASUS_DOCKER_ENV_DIR}/docker-builder-base.sh
 
-if [ -x "$(command -v docker)" ]; then
-    echo "Docker is installed and the execute permission is granted."
-    if getent group docker | grep &>/dev/null "\b$(id -un)\b"; then
-	echo "User $(id -un) is in the group docker."
-    else
-        echo "Docker is not managed as a non-root user."
-	echo "Please refer to the following URL to manage Docker as a non-root user."
-        echo "https://docs.docker.com/install/linux/linux-postinstall/"
-	exit
-    fi
-else
-    echo "Docker is not installed or the execute permission is not granted."
-    echo "Please refer to the following URL to install Docker."
-    echo "http://redmine.corpnet.asus/projects/configuration-management-service/wiki/Docker"
-    exit
-fi
+start_time=$SECONDS
 
-DIRECTORY_PATH_TO_DOCKER_BUILDER="$(dirname $(readlink -f $0))"
-echo "DIRECTORY_PATH_TO_DOCKER_BUILDER: $DIRECTORY_PATH_TO_DOCKER_BUILDER"
+asus_docker_env_main "$@"
 
-DIRECTORY_PATH_TO_SOURCE="$(dirname $DIRECTORY_PATH_TO_DOCKER_BUILDER)"
-
-if [ $# -eq 0 ]; then
-    echo "There is no directory path to the source provided."
-    echo "Use the default directory path to the source [$DIRECTORY_PATH_TO_SOURCE]."
-else
-    DIRECTORY_PATH_TO_SOURCE=$1
-    if [ ! -d $DIRECTORY_PATH_TO_SOURCE ]; then
-        echo "The source directory [$DIRECTORY_PATH_TO_SOURCE] is not found."
-        exit
-    fi
-fi
-
-DOCKER_IMAGE="asus/builder-tinker_board-android_n:latest"
-docker build --build-arg userid=$(id -u) --build-arg groupid=$(id -g) --build-arg username=$(id -un) -t $DOCKER_IMAGE \
-    --file $DIRECTORY_PATH_TO_DOCKER_BUILDER/Dockerfile $DIRECTORY_PATH_TO_DOCKER_BUILDER
-
-OPTIONS="--interactive --privileged --rm --tty"
-OPTIONS+=" --volume $DIRECTORY_PATH_TO_SOURCE:/source"
-echo "Options to run docker: $OPTIONS"
-
-if [ $BUILD_NUMBER ]; then
-	docker run $OPTIONS $DOCKER_IMAGE chroot --userspec=$USER:$USER / /bin/bash -c "cd /source; ./build.sh -n $BUILD_NUMBER"
-else
-	docker run $OPTIONS $DOCKER_IMAGE chroot --userspec=$USER:$USER / /bin/bash -c "cd /source; /bin/bash -i"
-fi
-
-end_time=$(date +"%s")
-tdiff=$(($end_time-$start_time))
-hours=$(($tdiff / 3600 ))
-mins=$((($tdiff % 3600) / 60))
-secs=$(($tdiff % 60))
-
-set +x
-
-echo -n "#### build completed "
-if [ $hours -gt 0 ] ; then
-	printf "(%02g:%02g:%02g (hh:mm:ss))" $hours $mins $secs
-elif [ $mins -gt 0 ] ; then
-	printf "(%02g:%02g (mm:ss))" $mins $secs
-elif [ $secs -gt 0 ] ; then
-	printf "(%s seconds)" $secs
-fi
-echo " ####"
+elapsed=$(( SECONDS - start_time ))
+eval "echo Elapsed time: $(date -ud "@$elapsed" +'$((%s/3600/24)) days %H hr %M mins %S secs')"
